e865d0511999af85ec3dc872221d868ad2bc42c3
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-07-16 02:27:17 +0600

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-// Main class implementing ActionListener for handling button events
+// Main class implementing ActionListener for handling button events.
 public class TicTacToe implements ActionListener {
 
     Random random = new Random(); // For randomly selecting which player starts
