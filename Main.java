56e646a516bd5cbaf00f7887e3a1b7262d779376
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-09-22 03:14:33 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 //This is main class
 
-public class Main {
+public class Main { 
     public static void main(String[] args) {
-        TicTacToe ticTacToe=new TicTacToe();
+        TicTacToe ticTacToe=new TicTacToe(); 
 
     }
 }
